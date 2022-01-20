feec72f0c5b1764dccbcc10868b78ff366f705af
Ja90n <81578391+Ja90n@users.noreply.github.com>
2022-01-20 17:02:42 +0100

    MLG troll added

--- src/main/java/com/blub/consoletrollplugin/Consoletrollplugin.java
+++ src/main/java/com/blub/consoletrollplugin/Consoletrollplugin.java
@@ -9,12 +9,15 @@ public final class Consoletrollplugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Bukkit.getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "Boom plugin has been loaded!");
+        getCommand("mlg").setExecutor(new mlgCommand());
+        Bukkit.getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "---------------------------------");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "| Starting ConsoleTrollPlugin v1.0 |");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "---------------------------------");
+        Bukkit.getConsoleSender().sendMessage(" ");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-
     }
 }
--- src/main/java/com/blub/consoletrollplugin/mlgCommand.java
+++ src/main/java/com/blub/consoletrollplugin/mlgCommand.java
@@ -2,30 +2,56 @@ package com.blub.consoletrollplugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class mlgCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(args.length == 2)) {
-            sender.sendMessage(ChatColor.RED + "Invalid arguments, try: /mlg <Player> <height of mlg>");
-        } else {
-            Player target = Bukkit.getPlayer(args[0]);
-            if (target != null) {
+        if (args.length == 1){
+            try {
+                Player target = Bukkit.getPlayer(args[0]);
+                target.sendMessage(ChatColor.LIGHT_PURPLE + "Je bent " + ChatColor.WHITE + "50" + ChatColor.LIGHT_PURPLE + " blocken de lucht in gestuurt, succes met de mlg");
+                target.teleport(target.getLocation().add(0, 50, 0));
+                try {
+                    ItemStack hand = target.getInventory().getItemInMainHand();
+                    try {
+                        target.getWorld().dropItem(target.getLocation(), hand);
+                    } catch (Exception e) {}
+                    target.getInventory().setItemInMainHand(new ItemStack(Material.WATER_BUCKET));
+                } catch (Exception e){}
+            } catch (NullPointerException e){
                 sender.sendMessage(ChatColor.RED + "You need to fill in the name of an online player!");
             }
+        } else if (args.length == 2){
             try {
-                int height = Integer.parseInt(args[0]);
-                target.teleport(target.getLocation().add(0, height, 0));
-
-            } catch (IllegalArgumentException e) {
-                sender.sendMessage(ChatColor.RED + "You need to fill in a number");
+                Player target = Bukkit.getPlayer(args[0]);
+                try {
+                    int height = Integer.parseInt(args[1]);
+                    target.teleport(target.getLocation().add(0,height,0));
+                    target.sendMessage(ChatColor.LIGHT_PURPLE + "Je bent " + ChatColor.WHITE + height + ChatColor.LIGHT_PURPLE +" blocken de lucht in gestuurt, succes met de mlg");
+                    try {
+                        ItemStack hand = target.getInventory().getItemInMainHand();
+                        try {
+                            target.getWorld().dropItem(target.getLocation(), hand);
+                        } catch (Exception e) {}
+                        target.getInventory().setItemInMainHand(new ItemStack(Material.WATER_BUCKET));
+                    } catch (Exception e){}
+                } catch (IllegalArgumentException e){
+                    target.sendMessage(ChatColor.RED + "You need to fill in a number");
+                }
+            } catch (NullPointerException e){
+                sender.sendMessage(ChatColor.RED + "You need to fill in the name of an online player!");
             }
+        } else {
+            sender.sendMessage(ChatColor.RED + "Invalid arguments, try: /mlg <player> <height>");
         }
         return false;
     }
 }
+
